e81c6d57d80538e2981207c322c9731257c1e26c
Nikita Vorotilin <vorotilin@mail.ru>
2023-01-12 22:49:09 +0300

    Create project

---
+++ M16-1/M16-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M16-1
+//
+//  Created by Nikita Vorotilin on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
