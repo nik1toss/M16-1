74f65aebebf43ade756adf4ee78c0b0a5c4919ed
Nikita Vorotilin <vorotilin@mail.ru>
2023-01-12 22:53:25 +0300

    Commit 2

--- M16-1/M16-1/ViewController.swift
+++ M16-1/M16-1/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("commit 1")
+        print("commit 2")
     }
 
 
